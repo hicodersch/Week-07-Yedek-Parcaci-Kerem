9bbd006d3077518452c210258599f9b4a79e49dd
Mehmet Dogan <info@intelligentworks.ch>
2020-12-26 22:17:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,20 +4,35 @@
 const carParts = ["tsla342", "vw0929", "tyt567", "bmw6126", "cabrio213", "alfaromeo3234"];
 
 // Parca isimleri büyük harflere cevrilecektir.
-const capitalize = (pName) => pName.toUpperCase();
-// Parca isimlerinden sayilar cikartilacaktir.
-const extractNumbers = (pName) => pName.replace(/[0-9]/g, '');
-// Parca isimleri tersine cevrilecektir.
-const reverse = (pName) => pName.split("").reverse().join("");
-// Her parcanin basina KEREMAG_ eklenecektir.
-const addCompanyName = (pName) => "KEREMAG__" + pName;
-// Her parcanin sonuna ise parcanin sisteme girildigi tarih eklenecektir.
-const addDate = (pName) => pName + "__" + Date();
+function capitalize(pName){ 
+  return pName.toUpperCase();
+}
 
-const result = carParts.map(capitalize)
-                       .map(reverse)
-                       .map(addCompanyName)
-                       .map(extractNumbers)
-                       .map(addDate);
+// Parca isimlerinden sayilar cikartilacaktir.
+function extractNumbers(pName){
+  return pName.replace(/[0-9]/g, '');
+}
+
+// Parca isimleri tersine cevrilecektir.
+function reverse(pName){
+  return pName.split("").reverse().join("");
+}
+
+// Her parcanin basina KEREMAG_ eklenecektir.
+function addCompanyName(pName){
+   return "KEREMAG__" + pName;
+}
+
+// Her parcanin sonuna ise parcanin sisteme girildigi tarih eklenecektir.
+function addDate(pName){
+  return pName + "__" + Date();
+}
+
+
+for(let index = 0; index < carParts.length; index++){
+  let nextPart = carParts[index];
+  cartParts[index] = capitalize(extractNumbers(reverse(addCompanyName(addDate(nextPart)))));  
+}
+
                        
 console.log("Spare Parts in Kerem AG", result);
